a258eb776bfec3e9b94a89fc19b651d172acb775
Norberto Caballero <norbertocaballero25@gmail.com>
2022-01-05 15:46:54 -0300

    Hola

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository //Le decimos a la clase PostRepository que va implementar la interfaz IPostRepository
+    {
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+
+            });
+
+            await Task.Delay(10);
+
+            return posts;
+        }
+
+       
+    }
+}
